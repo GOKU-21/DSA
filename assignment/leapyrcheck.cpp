a77a973a994aeac661c8e3b8988d49e5d7ad9dda
GOKU <132120494+GOKU-21@users.noreply.github.com>
2023-05-09 13:08:54 +0530

    Update leapyrcheck.cpp

--- assignment/leapyrcheck.cpp
+++ assignment/leapyrcheck.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+ #include<bits/stdc++.h>
 using namespace std;
 
 int main (){
@@ -6,17 +6,17 @@ int main (){
     cout << "enter a year: ";
     cin >> n;
     
-    if( n%400==0 && n%4==0){/*If a Year is divisible by 400,
+    if( n%400==0 && n%100==0){/*If a Year is divisible by 400,
      then it will be considered as a Leap Century Year.
     */
         cout << "  a LEAP century";
     }
-    else if(n%4==0){
+    else if(n%4==0 && n%100!=0){
         cout << " leap year ";
 
     }
     else {
-        cout << "neither leap nor century ";
+        cout << "not a leap year ";
     }
     return 0;
 }
